e3bef22883d1ab9339c6ac4e14c7df771fb00562
James Carlson <jxxcarlson@mac.com>
2018-01-16 02:50:43 +0800

    Bump version to 2.1: added Driver.parse and HasMath module.

--- src/MiniLatex/Driver.elm
+++ src/MiniLatex/Driver.elm
@@ -13,7 +13,7 @@ module MiniLatex.Driver
 
 # API
 
-@docs render, setup, getRenderedText, update, emptyEditRecord
+@docs render, setup, getRenderedText, parse, update, emptyEditRecord
 
 -}
 
@@ -52,6 +52,8 @@ render macroDefinitions text =
     MiniLatexDiffer.initialize emptyLatexState text |> getRenderedText macroDefinitions
 
 
+{-| parse takes a string of MiniLatex as input and creates the ccrresponding AST.
+-}
 parse : String -> List (List LatexExpression)
 parse text =
     text
---
+++ src/MiniLatex/HasMath.elm
@@ -0,0 +1,50 @@
+module MiniLatex.HasMath exposing (hasMath, listHasMath)
+
+import MiniLatex.Parser exposing (LatexExpression(..))
+
+
+{- Has Math code -}
+
+
+{-| Determine whether a (List LatexExpression) has math text in it
+-}
+listHasMath : List LatexExpression -> Bool
+listHasMath list =
+    list |> List.foldr (\x acc -> hasMath x || acc) False
+
+
+{-| Determine whether a LatexExpression has math text in it
+-}
+hasMath : LatexExpression -> Bool
+hasMath expr =
+    case expr of
+        LXString str ->
+            False
+
+        Comment str ->
+            False
+
+        Item k expr ->
+            hasMath expr
+
+        InlineMath str ->
+            True
+
+        DisplayMath str ->
+            True
+
+        Macro str expr ->
+            expr |> List.foldr (\x acc -> hasMath x || acc) False
+
+        Environment str expr ->
+            envHasMath str expr
+
+        LatexList list ->
+            list |> List.foldr (\x acc -> hasMath x || acc) False
+
+
+{-| Determine whether an environment has math in it
+-}
+envHasMath : String -> LatexExpression -> Bool
+envHasMath envType expr =
+    List.member envType [ "equation", "align", "eqnarray" ] || hasMath expr
